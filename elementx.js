8f9388a367e0838010cb967da2d8cf587d67eb61
Galizé <engajeturb@gmail.com>
2025-10-21 07:12:52 -0300

    Add files via upload

---
+++ elementx.js
@@ -0,0 +1,313 @@
+(function () {
+  const _0xe1320b = {
+    'brandName': "ZapyHub",
+    'version': "3.8.0.9",
+    'logo': {
+      'old': "https://app.lotsofwms.in/assets/images/custom/SandroOliveira/SandroOliveira.png",
+      'new': "https://app.lotsofwms.in/assets/images/custom/SandroOliveira/SandroOliveira.png"
+    },
+    'whatsapp': {
+      'oldNumber': "556592739631",
+      'newNumber': "556592739631"
+    },
+    'customDomain': "pro.hozte.com.br",
+    'links': {
+      'tutorials': "https://wa.me/556592739631",
+      'requestFeature': "https://wa.me/556592739631",
+      'privacyPolicy': "https://wa.me/556592739631"
+    },
+    'priceSymbolOld': "USD",
+    'priceSymbolNew': 'R$',
+    'supportMessageEN': "Hello there! I am facing an issue with Extension. I need your assistance.",
+    'supportMessagePT': "Quero Saber Mais sobre o ZapFlow"
+  };
+  const _0x5a696f = {
+    'Botzo': "ZapyHub",
+    "Extensão CRM": "ZapyHub 3.8.0.9",
+    "Bem-vindo ao CRM": "Bem-vindo ao ZapyHub 3.8.0.9",
+    'Loading...': "Carregando...",
+    "Monthly Plan": "Plano Mensal",
+    "Yearly Plan": "Plano Anual",
+    "WhatsApp CRM": "ZapyHub",
+    'puppyCRM': "ZapyHub",
+    "Premium 3 Days Trial": "- 3 Dias de Teste",
+    "Message automation": "Automação de mensagens",
+    "Unlimited messaging": "Mensagens ilimitadas",
+    "Contact tagging and segmentation functionality": "Etiquetagem e segmentação de contatos",
+    "Unlimited contact management": "Gerenciamento ilimitado de contatos",
+    "Full conversation history": "Histórico completo de conversas",
+    "Detailed reports and analysis": "Relatórios e análises detalhados",
+    "Advanced integration with WhatsApp": "Integração avançada com WhatsApp",
+    "Priority support via email and live chat": "Suporte prioritário por e-mail e chat ao vivo",
+    "All features of the free plan": "Todos os recursos do plano gratuito",
+    "R$ Get offer": "R$ Obter Oferta",
+    "Get offer": "Obter Oferta",
+    "Obtén oferta": "Obter Oferta",
+    'buy': "COMPRAR",
+    'USD': 'R$',
+    "License Details": "Detalhes da Licença",
+    "Here you will find detailed information about your license and options to uninstall it if necessary.": "Aqui você encontrará informações detalhadas sobre sua licença e opções para desinstalá-la, se necessário.",
+    'User:': "Usuário:",
+    'Phone:': "Telefone:",
+    'License:': "Licença:",
+    'Plan:': "Plano:",
+    "Next Payment:": "Próximo Pagamento:",
+    "Uninstall License": "Desinstalar Licença",
+    'Close': "Fechar",
+    "You must have Valid & Active License Key": "Você precisa de uma Chave de Licença Válida e Ativa",
+    "Enter your license key & click on Activate to Activate your License Key.": "Insira sua chave de licença e clique em Ativar para ativar sua chave de licença.",
+    "Activate Now": "Ativar Agora",
+    "You must enter a key": "Você deve inserir uma chave",
+    "Please enter your license key in the field below to activate advanced features.": "Insira sua chave de licença no campo abaixo para ativar recursos avançados.",
+    'undefined': "indefinido",
+    'Sort': "Organizar",
+    'Validate': "VALIDAR",
+    'Done': "Concluído",
+    "To Do": "A Fazer",
+    'Processing': "Processando",
+    'Cancel': "Cancelar",
+    'OK': 'OK',
+    'Column': "Coluna",
+    "Digite o Nome da Coluna:": "Digite o Nome da Coluna:",
+    'Adicionar': "Adicionar",
+    'Excluir': "Excluir",
+    'Inbox': "Caixa de Entrada",
+    'Perto': "Fechar",
+    "Recent 50 Chats": "50 Chats Recentes",
+    "Automação de mensagensMensagens ilimitadasEtiquetagem e segmentação de contatosGerenciamento ilimitado de contatosHistórico completo de conversasRelatórios e análises detalhadosIntegração avançada com WhatsAppSuporte prioritário por e-mail e chat ao vivoTodos os recursos do plano gratuito": "Recursos incluídos:<br>✅ Mensagens ilimitadas<br>✅ Automação de mensagens<br>✅ Etiquetagem e segmentação de contatos<br>✅ Gerenciamento ilimitado de contatos<br>✅ Histórico completo de conversas<br>✅ Relatórios e análises detalhados<br>✅ Integração avançada com o WhatsApp<br>✅ Suporte prioritário por e-mail e chat ao vivo<br>✅ Todos os recursos do plano gratuito incluídos",
+    "Your private user panel": "Seu painel de usuário privado",
+    'Tools': "Ferramentas",
+    "Sets of free functions": "Conjuntos de funções gratuitas",
+    'Functions': "Funções",
+    "Import and export contacts, personalized links, and much more...": "Importe e exporte contatos, links personalizados e muito mais...",
+    "Kanban Board": "Quadro Kanban",
+    "Organize all your clients with this powerful tool": "Organize todos os seus clientes com esta ferramenta poderosa",
+    "Quick Reply": "Resposta Rápida",
+    "Reply quickly": "Responda rapidamente",
+    "Scheduled Notifications": "Notificações Agendadas",
+    "Notify a customer in the future": "Notifique um cliente no futuro",
+    "Outgoing Webhooks": "Webhooks de Saída",
+    "Connect with Outer World": "Conecte-se com o Mundo Exterior",
+    'Chatbots': "Chatbots",
+    "Create and customize your autoresponder chatbot": "Crie e personalize seu chatbot de resposta automática",
+    "Scheduled Broadcasts": "Transmissões Agendadas",
+    "Schedule all your messages": "Agende todas as suas mensagens",
+    'Templates': "Modelos",
+    "Create and organize your templates": "Crie e organize seus modelos",
+    "Flash Broadcasts": "Transmissões Rápidas",
+    "Quick and emergency broadcasts": "Transmissões rápidas e de emergência",
+    "Custom Tabs": "Abas Personalizadas",
+    "Loading libraries…": "Carregando bibliotecas...",
+    "Choose Your Plan": "Escolha Seu Plano",
+    "If you already have a license, click here to access:": "Se você já possui uma licença, clique aqui para acessar:",
+    "Enter key": "Inserir chave",
+    'Month': "Mensal",
+    'Year': "Anual",
+    'Buy': "Comprar",
+    "Just a moment, we’ll be done shortly": "Aguarde um momento",
+    "Is your license expired? Don't worry, renew easily and go back to enjoying all the benefits. Keep your access active and avoid interruptions in your service. Click now to renew and continue without problems!": "Sua licença expirou? Não se preocupe, renove facilmente e volte a aproveitar todos os benefícios. Mantenha seu acesso ativo e evite interrupções no serviço. Clique agora para renovar e continuar sem problemas!",
+    "Renew License": "Renovar Licença",
+    "Pair your labels and organize like a pro": "Emparelhe suas etiquetas e organize como um profissional",
+    "The following parameters are available based on the action that triggers the webhook": "Os seguintes parâmetros estão disponíveis com base na ação que aciona o webhook",
+    "Specifies the action that triggered the webhook": "Especifica a ação que acionou o webhook",
+    "Unique identifier for the message": "Identificador exclusivo para a mensagem",
+    "Indicates the type of conversation, such as": "Indica o tipo de conversa, como",
+    'group': "grupo",
+    "or others": "ou outros",
+    "Unique identifier for the chat where the message was sent": "Identificador exclusivo do chat para onde a mensagem foi enviada",
+    "Unique identifier for the": "Identificador exclusivo do",
+    "where the message was sent": "para onde a mensagem foi enviada",
+    "Contains the data attached to the message, typically in base64 format": "Contém os dados anexados à mensagem, normalmente no formato base64",
+    "The content of the message": "O conteúdo da mensagem",
+    "Additional text or description accompanying the message": "Texto adicional ou descrição acompanhando a mensagem",
+    "Indicates whether the conversation is a": "Indica se a conversa é um",
+    "or not": "ou não",
+    "The sender of the message": "O remetente da mensagem",
+    "The recipient of the message": "O destinatário da mensagem",
+    "The name associated with the sender": "O nome associado ao remetente",
+    "The display name of the sender on WhatsApp": "O nome de exibição do remetente no WhatsApp",
+    "The name of the": "O nome do",
+    "where the message was sent": "para onde a mensagem foi enviada",
+    "The timestamp when the message was sent, expressed in seconds since the Unix epoch": "O registro de data e hora em que a mensagem foi enviada, expresso em segundos desde a era Unix"
+  };
+  function _0x179eaf() {
+    document.querySelectorAll("*:not(script):not(style)").forEach(_0x28a482 => {
+      _0x28a482.childNodes.forEach(_0x1f37ce => {
+        if (_0x1f37ce.nodeType === Node.TEXT_NODE && _0x1f37ce.nodeValue) {
+          Object.entries(_0x5a696f).forEach(([_0x3245b1, _0x32b85a]) => {
+            if (_0x1f37ce.nodeValue.includes(_0x3245b1) && !_0x1f37ce.nodeValue.includes(_0x32b85a)) {
+              if (_0x32b85a.includes("<br>")) {
+                const _0xc4e3a3 = document.createElement("span");
+                _0xc4e3a3.innerHTML = _0x1f37ce.nodeValue.replace(new RegExp(_0x3245b1, 'g'), _0x32b85a);
+                _0x1f37ce.replaceWith(_0xc4e3a3);
+              } else {
+                _0x1f37ce.nodeValue = _0x1f37ce.nodeValue.replace(new RegExp(_0x3245b1, 'g'), _0x32b85a);
+              }
+            }
+          });
+        }
+      });
+    });
+  }
+  function _0x52ecb2() {
+    document.querySelectorAll("img[src=\"" + _0xe1320b.logo.old + "\"]").forEach(_0x4d5047 => {
+      _0x4d5047.src = _0xe1320b.logo["new"];
+    });
+  }
+  function _0x38d80f() {
+    const _0x33beae = ["https://web.whatsapp.com/[object%20Object]", "https://wa.me/#?text=WA%20CRM%201%20Days%20Trail", "https://web.whatsapp.com/#", "https://https//zapyhub.com/buy/precios", "https://https//inftag.com/buyninjaflow/precios", "https://web.whatsapp.com/undefined", "https://pro.hozte.com.br/feedback"];
+    document.querySelectorAll('a').forEach(_0x111ff9 => {
+      _0x33beae.forEach(_0xd68a5b => {
+        if (_0x111ff9.href.includes(_0xd68a5b)) {
+          _0x111ff9.href = "pro.hozte.com.br";
+        }
+      });
+      if (_0x111ff9.href.includes("plwdesign.online")) {
+        _0x111ff9.href = _0x111ff9.href.replace(/plwdesign\.online/g, "pro.hozte.com.br");
+      }
+      if (_0x111ff9.href.includes("web.whatsapp.com") || _0x111ff9.href.includes("lotsofcode.in/precios")) {
+        _0x111ff9.href = "pro.hozte.com.br".startsWith("http") ? "pro.hozte.com.br" : "https://pro.hozte.com.br";
+      }
+    });
+  }
+  function _0x3b1957() {
+    const {
+      tutorials: _0x3c5fe8,
+      requestFeature: _0x48b3aa,
+      privacyPolicy: _0x15c35a
+    } = _0xe1320b.links;
+    document.querySelectorAll('a').forEach(_0x304c4b => {
+      if (_0x304c4b.href.includes("/tutorials")) {
+        _0x304c4b.href = _0x3c5fe8;
+      }
+      if (_0x304c4b.href.includes("/request-feature")) {
+        _0x304c4b.href = _0x48b3aa;
+      }
+      if (_0x304c4b.href.includes("/privacy-policy")) {
+        _0x304c4b.href = _0x15c35a;
+      }
+    });
+  }
+  function _0x2e7741() {
+    document.querySelectorAll("div, span, p").forEach(_0x2c03c9 => {
+      if (_0x2c03c9.innerText.includes("USD")) {
+        _0x2c03c9.innerText = _0x2c03c9.innerText.replace(new RegExp("USD", 'g'), 'R$');
+      }
+    });
+  }
+  function _0x37bcb8() {
+    const {
+      oldNumber: _0x453a73,
+      newNumber: _0x7d0e2a
+    } = _0xe1320b.whatsapp;
+    document.querySelectorAll("a[href*='wa.me']").forEach(_0x54b285 => {
+      if (_0x54b285.href.includes(_0x453a73)) {
+        _0x54b285.href = _0x54b285.href.replace(_0x453a73, _0x7d0e2a);
+      }
+      _0x54b285.addEventListener("click", () => {
+        if (_0x54b285.href.includes(_0x453a73)) {
+          _0x54b285.href = _0x54b285.href.replace(_0x453a73, _0x7d0e2a);
+        }
+      });
+    });
+  }
+  function _0x49ee8f() {
+    let _0x4d29d9 = 0;
+    const _0x2f59d8 = setInterval(() => {
+      _0x37bcb8();
+      if (++_0x4d29d9 >= 10) {
+        clearInterval(_0x2f59d8);
+      }
+    }, 1000);
+  }
+  function _0x47858f() {
+    const {
+      supportMessageEN: _0x3ed75d,
+      supportMessagePT: _0x2a2f3a
+    } = _0xe1320b;
+    document.querySelectorAll("*:not(script):not(style)").forEach(_0x3e5f79 => {
+      _0x3e5f79.childNodes.forEach(_0x566500 => {
+        if (_0x566500.nodeType === Node.TEXT_NODE && _0x566500.nodeValue?.["includes"](_0x3ed75d)) {
+          _0x566500.nodeValue = _0x566500.nodeValue.replace(new RegExp(_0x3ed75d, 'g'), _0x2a2f3a);
+        }
+      });
+    });
+  }
+  function _0x1f3af6() {
+    const _0x58bfb7 = {
+      "Aguarde um momento": true,
+      'Carregando': true,
+      "Carregando bibliotecas…": true,
+      "Aguarde um momento, logo estaremos prontos": true
+    };
+    document.querySelectorAll("*:not(script):not(style)").forEach(_0x41088b => {
+      const _0x2fac4a = _0x41088b.textContent.trim();
+      if (_0x58bfb7[_0x2fac4a]) {
+        _0x41088b.style.color = "black";
+      }
+    });
+  }
+  function _0x3a8b90() {
+    const _0x4bf970 = window.Store?.["SendTextMsgToChat"];
+    if (_0x4bf970) {
+      window.Store.SendTextMsgToChat = async function (_0x2d8917, ..._0x1b84fd) {
+        try {
+          if (_0x2d8917.isGroup && !_0x2d8917.id.server.includes("g.us")) {
+            _0x2d8917.id.server = "g.us";
+          }
+          return _0x4bf970.apply(this, [_0x2d8917, ..._0x1b84fd]);
+        } catch (_0x255f10) {
+          console.error("Erro ao corrigir envio para grupos:", _0x255f10);
+        }
+      };
+    }
+  }
+  function _0x57baf2() {
+    document.querySelectorAll("a[href*=\"pro.hozte.com.br/feedback\"]").forEach(_0x2d0812 => {
+      _0x2d0812.href = "pro.hozte.com.br";
+      _0x2d0812.addEventListener("click", _0x490fec => {
+        _0x490fec.preventDefault();
+        window.open("pro.hozte.com.br", "_blank");
+      });
+    });
+  }
+  function _0x3f01de() {
+    const _0x143d07 = new MutationObserver(() => {
+      _0x179eaf();
+      _0x52ecb2();
+      _0x38d80f();
+      _0x3b1957();
+      _0x2e7741();
+      _0x37bcb8();
+      _0x47858f();
+      _0x1f3af6();
+      _0x57baf2();
+    });
+    _0x143d07.observe(document.body, {
+      'childList': true,
+      'subtree': true
+    });
+  }
+  function _0x2a4392() {
+    const _0x3a7199 = () => {
+      _0x179eaf();
+      _0x52ecb2();
+      _0x38d80f();
+      _0x3b1957();
+      _0x2e7741();
+      _0x37bcb8();
+      _0x49ee8f();
+      _0x47858f();
+      _0x3a8b90();
+      _0x1f3af6();
+      _0x57baf2();
+      _0x3f01de();
+    };
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", _0x3a7199);
+    } else {
+      _0x3a7199();
+    }
+  }
+  _0x2a4392();
+})();
